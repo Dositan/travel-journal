772e0257292cf182a99a1d9babfe3df999dccf6c
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-06-23 10:03:35 +0600

    ✅ Update `data.js`

--- src/data.js
+++ src/data.js
@@ -1,17 +1,6 @@
 const data = [
   {
     id: 1,
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
-    description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
-  },
-  {
-    id: 2,
     title: "Kolsai Lakes",
     location: "Kazakhstan",
     googleMapsUrl: "https://goo.gl/maps/GJfDrRGH3XnSSMKG6",
@@ -23,7 +12,7 @@ const data = [
       "https://images.unsplash.com/photo-1606300314595-51acfb6240f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
   },
   {
-    id: 3,
+    id: 2,
     title: "Lake Kaindy",
     location: "Kazakhstan",
     googleMapsUrl: "https://goo.gl/maps/wdEsD3ssY8c8GNB97",
@@ -33,6 +22,17 @@ const data = [
       "Lake Kaindy is an amazing creation of nature, included in the fifteen beautiful lakes of the planet. Many travelers dream to see because it is where tourists can witness amazing natural phenomenon - straight from the mountain lake some mystical peaks rise 15 meters high, which looks like a wrecked ship matches...",
     imageUrl:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.eMjN4rejX_o2jOMyG4e0twHaJ4%26pid%3DApi&f=1",
+  },
+  {
+    id: 3,
+    title: "Sharm El-Sheikh",
+    location: "Egypt",
+    googleMapsUrl: "https://goo.gl/maps/PghKMs1j9AmMAmnQ6",
+    startDate: "1 Jul, 2022",
+    endDate: "14 Jul, 2022",
+    description:
+      "Coming soon.",
+    imageUrl: "https://images.unsplash.com/photo-1562972661-a704b05a8a05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
   },
 ];
 
