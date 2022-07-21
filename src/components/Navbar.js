a791c5e6151a4297ce4fb055ea67721e70ee55e8
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-22 00:12:23 +0600

    feat(*): add links to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,20 @@ import "../styles/Navbar.css";
 function Navbar() {
   return (
     <nav>
-      <img className="nav--logo" src="/images/logo.png" alt="logo" />
-      <h3 className="nav--title">my travel journal.</h3>
+      <ul>
+        <li className="logo">
+          <img src="/images/logo.png" alt="logo" />
+          my travel journal.
+        </li>
+        <div className="links">
+          <li>
+            <a href="https://dositan-homepage.vercel.app/">Portfolio</a>
+          </li>
+          <li>
+            <a href="https://instagram.com/dastanozgeldi/">Instagram</a>
+          </li>
+        </div>
+      </ul>
     </nav>
   );
 }
